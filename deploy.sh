fe80d24252e929a89ea252c2fa2b4d820c8b27f9
chilinet <sluther@chilinet.solutions>
2025-03-16 08:17:30 +0100

    Add Azure deployment configuration

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ----------------------
+# KUDU Deployment Script
+# Version: 1.0.17
+# ----------------------
+
+# Installs dependencies and builds the Next.js application
+npm install
+npm run build 
