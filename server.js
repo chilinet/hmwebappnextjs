b4654e6c256b407063ed9477284957b6097c240d
chilinet <sluther@chilinet.solutions>
2025-03-16 11:50:45 +0100

    Add Azure deployment configuration 6

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  console.log('App prepared')
+  console.log('App prepared 2')
   createServer(async (req, res) => {
     try {
       const parsedUrl = parse(req.url, true)
