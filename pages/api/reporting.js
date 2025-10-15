641ecc1affc02c8a7968c367471be611d71e839f
chilinet <sluther@chilinet.solutions>
2025-10-15 21:34:00 +0200

    reporting api 2

--- pages/api/reporting.js
+++ pages/api/reporting.js
@@ -1,4 +1,4 @@
-import { getPgConnection } from '../../../lib/pgdb.js';
+import { getPgConnection } from '../../lib/pgdb.js';
 
 // Preshared Key für Authentifizierung
 const PRESHARED_KEY = process.env.REPORTING_PRESHARED_KEY || 'default-reporting-key-2024';
